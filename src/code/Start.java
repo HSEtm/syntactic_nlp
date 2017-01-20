955b78b3f5786821f9cd65ea766b2cf38b131e35
pbakhtin <bakhtinpavel.24@gmail.com>
2017-01-20 13:56:50 +0300

    Added sentence word count for filtering very long texts that may cause Java Out of Memory Exception

--- src/code/Start.java
+++ src/code/Start.java
@@ -27,7 +27,7 @@ import edu.stanford.nlp.semgraph.SemanticGraphEdge;
 public class Start {
 
 	public static void main(String[] args) {
-//		args = new String[] { "input", "true" };
+//		args = new String[] { "input/3.txt", "true" };
 
 		Logging logging = new Logging();
 		logging.setLogErrors(false);
@@ -115,124 +115,210 @@ public class Start {
 									List<NLPNode[]> sentences = decoder.decodeDocument(in);
 
 									for (NLPNode[] nodes : sentences) {
-										try {
-											StringBuilder sb = new StringBuilder();
+//										System.out.println(nodes.length);
+										if (nodes.length <= 500) {
+											try {
+												StringBuilder sb = new StringBuilder();
 
-											for (int g = 1; g < nodes.length; g++) {
-												sb.append(nodes[g].getWordForm() + " ");
-											}
-											
-											String sentenceStr = sb.toString();
-											
-											// list of symbols from http://www.fileformat.info/info/unicode/category/Pd/list.htm
-											sentenceStr = sentenceStr
-													.replaceAll("[\u002D\u058A\u05BE\u1400\u1806\u2010\u2011\u2012\u2013\u2014\u2015\u2E17\u2E1A\u2E3A\u2E3B\u2E40\u301C\u3030\u30A0\uFE31\uFE32\uFE58\uFE63\uFF0D]", "-")
-													.replace('\u2010', '-').replace('\u2011', '-')
-													.replace('\uFFFD', '-').replace('\u202F', '-')
-													.replace(" - ", "-").replace(" / ", "/").trim();
-
-											if (sentenceStr.length() > 0) {
-												Annotation sentence = new Annotation(sentenceStr);
-
-												pipeline.annotate(sentence);
-
-												SemanticGraph dependencies = sentence
-														.get(CoreAnnotations.SentencesAnnotation.class).get(0)
-														.get(CollapsedCCProcessedDependenciesAnnotation.class);
-
-												String rootToken = dependencies.getFirstRoot().word();
-												String rootLemma = dependencies.getFirstRoot().lemma();
-												String rootPos = dependencies.getFirstRoot().backingLabel()
-														.get(CoreAnnotations.PartOfSpeechAnnotation.class).toString();
-												int rootDepid = dependencies.getFirstRoot().index();
-												String rootDeptype = "root";
-												String rootNer = "O";
-
-												switch (rootToken) {
-												case "-RSB-":
-													rootToken = "]";
-													rootLemma = "]";
-													break;
-
-												case "-LSB-":
-													rootToken = "[";
-													rootLemma = "[";
-													break;
-
-												case "-RRB-":
-													rootToken = ")";
-													rootLemma = ")";
-													break;
-
-												case "-LRB-":
-													rootToken = "(";
-													rootLemma = "(";
-													break;
-
-												case "-RCB-":
-													rootToken = "}";
-													rootLemma = "}";
-													break;
-
-												case "-LCB-":
-													rootToken = "{";
-													rootLemma = "{";
-													break;
+												for (int g = 1; g < nodes.length; g++) {
+													sb.append(nodes[g].getWordForm() + " ");
 												}
 
-												int i = 0;
+												String sentenceStr = sb.toString();
 
-												boolean rootOnly = true;
+												// list of symbols from http://www.fileformat.info/info/unicode/category/Pd/list.htm
+												sentenceStr = sentenceStr
+														.replaceAll("[\u002D\u058A\u05BE\u1400\u1806\u2010\u2011\u2012\u2013\u2014\u2015\u2E17\u2E1A\u2E3A\u2E3B\u2E40\u301C\u3030\u30A0\uFE31\uFE32\uFE58\uFE63\uFF0D]", "-")
+														.replace('\u2010', '-').replace('\u2011', '-')
+														.replace('\uFFFD', '-').replace('\u202F', '-')
+														.replace(" - ", "-").replace(" / ", "/").trim();
 
-												for (SemanticGraphEdge edge : dependencies.edgeListSorted()) {
-													rootOnly = false;
+												if (sentenceStr.length() > 0) {
+													Annotation sentence = new Annotation(sentenceStr);
 
-													String token = edge.getDependent().word();
-													String lemma = edge.getDependent().lemma();
-													String pos = edge.getDependent().backingLabel()
-															.get(CoreAnnotations.PartOfSpeechAnnotation.class)
-															.toString();
-													int depid = edge.getDependent().index();
-													int govid = edge.getGovernor().index();
-													String deptype = edge.getRelation().getShortName();
-													String deptype_add = edge.getRelation().toString();
-													String ner = "O";
+													pipeline.annotate(sentence);
 
-													switch (token) {
+													SemanticGraph dependencies = sentence
+															.get(CoreAnnotations.SentencesAnnotation.class).get(0)
+															.get(CollapsedCCProcessedDependenciesAnnotation.class);
+
+													String rootToken = dependencies.getFirstRoot().word();
+													String rootLemma = dependencies.getFirstRoot().lemma();
+													String rootPos = dependencies.getFirstRoot().backingLabel()
+															.get(CoreAnnotations.PartOfSpeechAnnotation.class).toString();
+													int rootDepid = dependencies.getFirstRoot().index();
+													String rootDeptype = "root";
+													String rootNer = "O";
+
+													switch (rootToken) {
 													case "-RSB-":
-														token = "]";
-														lemma = "]";
+														rootToken = "]";
+														rootLemma = "]";
 														break;
 
 													case "-LSB-":
-														token = "[";
-														lemma = "[";
+														rootToken = "[";
+														rootLemma = "[";
 														break;
 
 													case "-RRB-":
-														token = ")";
-														lemma = ")";
+														rootToken = ")";
+														rootLemma = ")";
 														break;
 
 													case "-LRB-":
-														token = "(";
-														lemma = "(";
+														rootToken = "(";
+														rootLemma = "(";
 														break;
 
 													case "-RCB-":
-														token = "}";
-														lemma = "}";
+														rootToken = "}";
+														rootLemma = "}";
 														break;
 
 													case "-LCB-":
-														token = "{";
-														lemma = "{";
+														rootToken = "{";
+														rootLemma = "{";
 														break;
 													}
 
-													int j = i;
+													int i = 0;
 
-													if (depid - rootDepid == 1) {
+													boolean rootOnly = true;
+
+													for (SemanticGraphEdge edge : dependencies.edgeListSorted()) {
+														rootOnly = false;
+
+														String token = edge.getDependent().word();
+														String lemma = edge.getDependent().lemma();
+														String pos = edge.getDependent().backingLabel()
+																.get(CoreAnnotations.PartOfSpeechAnnotation.class)
+																.toString();
+														int depid = edge.getDependent().index();
+														int govid = edge.getGovernor().index();
+														String deptype = edge.getRelation().getShortName();
+														String deptype_add = edge.getRelation().toString();
+														String ner = "O";
+
+														switch (token) {
+														case "-RSB-":
+															token = "]";
+															lemma = "]";
+															break;
+
+														case "-LSB-":
+															token = "[";
+															lemma = "[";
+															break;
+
+														case "-RRB-":
+															token = ")";
+															lemma = ")";
+															break;
+
+														case "-LRB-":
+															token = "(";
+															lemma = "(";
+															break;
+
+														case "-RCB-":
+															token = "}";
+															lemma = "}";
+															break;
+
+														case "-LCB-":
+															token = "{";
+															lemma = "{";
+															break;
+														}
+
+														int j = i;
+
+														if (depid - rootDepid == 1) {
+															while (!(rootToken.replace(".", "").trim()
+																	.toLowerCase().equals(nodes[i].getWordForm()
+																			.replace(".", "").trim().toLowerCase()))
+																	&& i < nodes.length - 1) {
+																i++;
+															}
+
+															if (rootToken.replace(".", "").trim()
+																	.toLowerCase().equals(nodes[i].getWordForm()
+																			.replace(".", "").trim().toLowerCase())
+																	&& i - j < 10) {
+																rootNer = nodes[i].getNamedEntityTag();
+																if (nodes[i].getLemma() == "#hlink#")
+																	rootLemma = nodes[i].getLemma();
+															} else
+																i = j;
+
+															try (FileWriter fw = new FileWriter(outputFile, true);
+																	BufferedWriter bw = new BufferedWriter(fw);
+																	PrintWriter out = new PrintWriter(bw)) {
+																out.println(docid + "\t" + sent_id + "\t" + rootToken + "\t"
+																		+ rootLemma + "\t" + rootPos + "\t" + rootDepid
+																		+ "\t" + 0 + "\t" + rootDeptype + "\t" + null + "\t"
+																		+ rootNer);
+															} catch (IOException e) {
+																e.printStackTrace();
+
+																if (logging.isLogErrors()) {
+																	logging.writeErrorLog(outputFile, e);
+																}
+															}
+
+														}
+
+														j = i;
+
+														while (!(token.replace(".", "").trim()
+																.toLowerCase().equals(nodes[i].getWordForm()
+																		.replace(".", "").trim().toLowerCase()))
+																&& i < nodes.length - 1) {
+															i++;
+														}
+
+														if (token.replace(".", "").trim()
+																.toLowerCase().equals(nodes[i].getWordForm()
+																		.replace(".", "").trim().toLowerCase())
+																&& i - j < 10) {
+															ner = nodes[i].getNamedEntityTag();
+															if (nodes[i].getLemma() == "#hlink#")
+																lemma = nodes[i].getLemma();
+														} else
+															i = j;
+
+														int symbol = deptype.indexOf(":");
+
+														if (symbol >= 0 && deptype == deptype_add) {
+															deptype = deptype.substring(0, symbol);
+														}
+
+														symbol = deptype_add.indexOf(":");
+
+														if (symbol >= 0) {
+															deptype_add = deptype_add.substring(symbol + 1);
+														} else
+															deptype_add = null;
+
+														try (FileWriter fw = new FileWriter(outputFile, true);
+																BufferedWriter bw = new BufferedWriter(fw);
+																PrintWriter out = new PrintWriter(bw)) {
+															out.println(docid + "\t" + sent_id + "\t" + token + "\t" + lemma
+																	+ "\t" + pos + "\t" + depid + "\t" + govid + "\t"
+																	+ deptype + "\t" + deptype_add + "\t" + ner);
+														} catch (IOException e) {
+															e.printStackTrace();
+
+															if (logging.isLogErrors()) {
+																logging.writeErrorLog(outputFile, e);
+															}
+														}
+													}
+
+													if (rootOnly) {
+														int j = i;
+
 														while (!(rootToken.replace(".", "").trim()
 																.toLowerCase().equals(nodes[i].getWordForm()
 																		.replace(".", "").trim().toLowerCase()))
@@ -254,8 +340,8 @@ public class Start {
 																BufferedWriter bw = new BufferedWriter(fw);
 																PrintWriter out = new PrintWriter(bw)) {
 															out.println(docid + "\t" + sent_id + "\t" + rootToken + "\t"
-																	+ rootLemma + "\t" + rootPos + "\t" + rootDepid
-																	+ "\t" + 0 + "\t" + rootDeptype + "\t" + null + "\t"
+																	+ rootLemma + "\t" + rootPos + "\t" + rootDepid + "\t"
+																	+ 0 + "\t" + rootDeptype + "\t" + null + "\t"
 																	+ rootNer);
 														} catch (IOException e) {
 															e.printStackTrace();
@@ -264,103 +350,20 @@ public class Start {
 																logging.writeErrorLog(outputFile, e);
 															}
 														}
-
 													}
 
-													j = i;
+													// System.out.println(docid + ":
+													// " +
+													// sent_id);
 
-													while (!(token.replace(".", "").trim()
-															.toLowerCase().equals(nodes[i].getWordForm()
-																	.replace(".", "").trim().toLowerCase()))
-															&& i < nodes.length - 1) {
-														i++;
-													}
-
-													if (token.replace(".", "").trim()
-															.toLowerCase().equals(nodes[i].getWordForm()
-																	.replace(".", "").trim().toLowerCase())
-															&& i - j < 10) {
-														ner = nodes[i].getNamedEntityTag();
-														if (nodes[i].getLemma() == "#hlink#")
-															lemma = nodes[i].getLemma();
-													} else
-														i = j;
-
-													int symbol = deptype.indexOf(":");
-
-													if (symbol >= 0 && deptype == deptype_add) {
-														deptype = deptype.substring(0, symbol);
-													}
-
-													symbol = deptype_add.indexOf(":");
-
-													if (symbol >= 0) {
-														deptype_add = deptype_add.substring(symbol + 1);
-													} else
-														deptype_add = null;
-
-													try (FileWriter fw = new FileWriter(outputFile, true);
-															BufferedWriter bw = new BufferedWriter(fw);
-															PrintWriter out = new PrintWriter(bw)) {
-														out.println(docid + "\t" + sent_id + "\t" + token + "\t" + lemma
-																+ "\t" + pos + "\t" + depid + "\t" + govid + "\t"
-																+ deptype + "\t" + deptype_add + "\t" + ner);
-													} catch (IOException e) {
-														e.printStackTrace();
-
-														if (logging.isLogErrors()) {
-															logging.writeErrorLog(outputFile, e);
-														}
-													}
+													sent_id++;
 												}
+											} catch (Exception e) {
+												e.printStackTrace();
 
-												if (rootOnly) {
-													int j = i;
-
-													while (!(rootToken.replace(".", "").trim()
-															.toLowerCase().equals(nodes[i].getWordForm()
-																	.replace(".", "").trim().toLowerCase()))
-															&& i < nodes.length - 1) {
-														i++;
-													}
-
-													if (rootToken.replace(".", "").trim()
-															.toLowerCase().equals(nodes[i].getWordForm()
-																	.replace(".", "").trim().toLowerCase())
-															&& i - j < 10) {
-														rootNer = nodes[i].getNamedEntityTag();
-														if (nodes[i].getLemma() == "#hlink#")
-															rootLemma = nodes[i].getLemma();
-													} else
-														i = j;
-
-													try (FileWriter fw = new FileWriter(outputFile, true);
-															BufferedWriter bw = new BufferedWriter(fw);
-															PrintWriter out = new PrintWriter(bw)) {
-														out.println(docid + "\t" + sent_id + "\t" + rootToken + "\t"
-																+ rootLemma + "\t" + rootPos + "\t" + rootDepid + "\t"
-																+ 0 + "\t" + rootDeptype + "\t" + null + "\t"
-																+ rootNer);
-													} catch (IOException e) {
-														e.printStackTrace();
-
-														if (logging.isLogErrors()) {
-															logging.writeErrorLog(outputFile, e);
-														}
-													}
+												if (logging.isLogErrors()) {
+													logging.writeErrorLog(outputFile, e);
 												}
-
-												// System.out.println(docid + ":
-												// " +
-												// sent_id);
-
-												sent_id++;
-											}
-										} catch (Exception e) {
-											e.printStackTrace();
-
-											if (logging.isLogErrors()) {
-												logging.writeErrorLog(outputFile, e);
 											}
 										}
 									}
